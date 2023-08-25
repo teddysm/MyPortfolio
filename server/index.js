4c4c31c8278778e67b06860d4c2aebbae6869926
Thanh Doan <doan.teddy@gmail.com>
2023-08-24 22:39:26 -0400

    testing

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 app.use(cors());
 app.use(bodyParser.json());
 
